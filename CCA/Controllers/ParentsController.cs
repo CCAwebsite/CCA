f14d2d9fea936f04efb5b73819be1249cf62d910
zerr_C <zerr_C@hcde.org>
2016-05-10 10:03:41 -0400

    Updated

---
+++ CCA/Controllers/ParentsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CCA.Controllers
+{
+    public class ParentsController : Controller
+    {
+        // GET: Parents
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public ActionResult Volunteer()
+        {
+            return View();
+        }
+        public ActionResult SummerSchool()
+        {
+            return View();
+        }
+        public ActionResult PAPA()
+        {
+            return View();
+        }
+    }
+}
---
+++ CCA/Controllers/StudentsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CCA.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CollegeAdvisor()
+        {
+            return View();
+        }
+        public ActionResult Counselors()
+        {
+            return View();
+        }
+        public ActionResult Faculty()
+        {
+            return View();
+        }
+        public ActionResult DualEnrollment()
+        {
+            return View();
+        }
+        public ActionResult PrepforSeniorYear()
+        {
+            return View();
+        }
+        public ActionResult SeniorFAQ()
+        {
+            return View();
+        }
+        public ActionResult Testing()
+        {
+            return View();
+        }
+    }
+}
