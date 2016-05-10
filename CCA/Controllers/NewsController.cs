51ee11d55707714471e21ad015a97c676de4053e
zerr_C <zerr_C@hcde.org>
2016-05-10 10:20:40 -0400

    Added News and Resources controllers

---
+++ CCA/Controllers/NewsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CCA.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Newsletters()
+        {
+            return View();
+        }
+        public ActionResult IntheNews()
+        {
+            return View();
+        }
+        public ActionResult Alumni()
+        {
+            return View();
+        }
+    }
+}
---
+++ CCA/Controllers/ResourcesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CCA.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        // GET: Resources
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GovernorsSchool()
+        {
+            return View();
+        }
+        public ActionResult PowerSchool()
+        {
+            return View();
+        }
+        public ActionResult SummerReadingLists()
+        {
+            return View();
+        }
+        public ActionResult SupplyLists()
+        {
+            return View();
+        }
+        public ActionResult TransitionMeeting()
+        {
+            return View();
+        }
+        public ActionResult UsefulWebsites()
+        {
+            return View();
+        }
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+    }
+}
